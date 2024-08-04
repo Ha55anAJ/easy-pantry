68922d8a0917bf17add1f49c237625294cd9acf5
HassanAJ81 <149829834+HassanAJ81@users.noreply.github.com>
2024-08-04 20:51:20 +0500

    updated

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -19,6 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-const auth = getAuth(app);
-export { firestore, auth }; 
+export const auth = getAuth(app);
--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,25 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+// // next.config.js
+// const path = require('path');
+
+// module.exports = {
+//   webpack: (config) => {
+//     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+//     return config;
+//   },
+// };
+
+// const path = require('path');
+
+// const nextConfig = {
+//   webpack: (config) => {
+//     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
